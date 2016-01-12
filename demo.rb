e308f2ae6d75f9b2f8cd01af03426cd4b8d5d0d9
Patrick Li <patrickli727@gmail.com>
2016-01-12 00:20:06 -0800

    has_many and belongs_to associations work.

--- demo.rb
+++ demo.rb
@@ -1,22 +1,45 @@
 require './lib/db_connect'
 require './lib/sql_object'
 require './lib/searchable'
+require './lib/associatable'
 
 class Musician < SQLObject
   self.finalize!
+
+  has_many(:favoriteartists,
+    foreign_key: :musician_id,
+    class_name: "FavoriteArtist"
+  )
+
+  belongs_to(:musicgroup,
+    foreign_key: :group_id,
+    class_name: "MusicGroup"
+  )
 end
 
 class MusicGroup < SQLObject
   self.finalize!
+
+  has_many(:musicians,
+    foreign_key: :group_id,
+    class_name: "Musician"
+  )
 end
 
 class FavoriteArtist < SQLObject
   self.finalize!
+
+  belongs_to(:musician,
+    foreign_key: :musician_id,
+    class_name: "Musician"
+  )
 end
 
-
-p "All musicians: #{Musician.all}"
-
-p "First musician's first name: #{Musician.find(1).fname}"
-
-p "Musician named Justin: #{Musician.where(fname: 'Justin')}"
+#
+# p "All musicians: #{Musician.all}"
+#
+# p "First musician's first name: #{Musician.find(1).fname}"
+#
+# p "Musician named Justin: #{Musician.where(fname: 'Justin')}"
+#
+p "Galantis's artists: #{MusicGroup.find(1).musicians}"
